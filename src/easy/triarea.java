6326aa077ce338455376b129c1e4330f62453951
Zeheng Li <imzehengl@gmail.com>
2022-12-16 15:27:42 -0700

    more examples

---
+++ src/easy/triarea.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+class TriangleArea {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        float a = s.nextFloat();
+        int b = s.nextInt();
+        System.out.println(a * b / 2);
+    }
+}

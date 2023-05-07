c97e38e6e701430b0b10b86db3ceeeb9f83cdee6
Zeheng Li <imzehengl@gmail.com>
2023-05-06 20:43:31 -0600

    clean up
    Co-authored-by: chez entity <chez-entity@users.noreply.github.com>
    Co-authored-by: Yzhao20100531 <Yzhao20100531@users.noreply.github.com>

--- src/easy/chocolatedivision.py
+++ src/easy/chocolatedivision.py
@@ -1,6 +1,6 @@
-R, C = [int(v) for v in input().split()]
+r, c = [int(v) for v in input().split()]
 
-chocolate_bar_cuts = (R * C)-1
+chocolate_bar_cuts = (r * c) - 1
 
 if chocolate_bar_cuts % 2 == 1:
     print("Alf")

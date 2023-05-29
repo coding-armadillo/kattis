08d714b576ef3d353ab3bd2b803bcfaec6551a4a
Zeheng Li <imzehengl@gmail.com>
2023-05-29 09:24:57 -0600

    clean up

--- src/easy/delimitersoup.py
+++ src/easy/delimitersoup.py
@@ -1,28 +1,27 @@
 n = int(input())
-D_s = input()
+s = input()
 
 stack = []
 
 valid = True
 
 for i in range(n):
-    c = D_s[i]
+    c = s[i]
     if c in ["(", "[", "{"]:
         stack.append(c)
     if c in [")", "]", "}"]:
         if len(stack) == 0:
             valid = False
             break
+
         v = stack.pop()
         if c == ")" and v != "(":
             valid = False
             break
-            
-        elif c == "]" and  v != "[":
+        elif c == "]" and v != "[":
             valid = False
             break
-            
-        elif c == "}" and  v != "{":
+        elif c == "}" and v != "{":
             valid = False
             break
 

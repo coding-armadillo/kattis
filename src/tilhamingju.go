06898e6d8741b1c12e0cf75fdf77519ca9e33085
Zeheng Li <imzehengl@gmail.com>
2024-08-11 23:14:26 -0600

    add examples

---
+++ src/tilhamingju.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("TIL HAMINGJU MED AFMAELID FORRITUNARKEPPNI FRAMHALDSSKOLANNA!")
+}
---
+++ src/tilhamingju.java
@@ -0,0 +1,5 @@
+class Tilhamingju {
+    public static void main(String[] args) {
+        System.out.println("TIL HAMINGJU MED AFMAELID FORRITUNARKEPPNI FRAMHALDSSKOLANNA!");
+    }
+}

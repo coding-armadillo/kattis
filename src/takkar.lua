bd9f62ad236018545cf6fdad56756d748ad67c15
Zeheng Li <imzehengl@gmail.com>
2024-08-28 11:44:43 -0600

    add examples

---
+++ src/takkar.lua
@@ -0,0 +1,9 @@
+a = io.read("*n")
+b = io.read("*n")
+if a < b then
+    print("FAKE NEWS!")
+elseif a == b then
+    print("WORLD WAR 3!")
+else
+    print("MAGA!")
+end
---
+++ src/takkar.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = int(input())
+if a < b:
+    print("FAKE NEWS!")
+elif a == b:
+    print("WORLD WAR 3!")
+else:
+    print("MAGA!")

184d2d5013d6121e286c93e809073923c9906004
Zeheng Li <imzehengl@gmail.com>
2025-01-21 10:24:20 -0700

    add examples

---
+++ src/euclidsalgorithm.lua
@@ -0,0 +1,7 @@
+a, b = io.read("*n", "*n")
+
+function gcd(a, b)
+	return b == 0 and a or gcd(b, a % b)
+end
+
+print(gcd(a, b))
---
+++ src/euclidsalgorithm.py
@@ -0,0 +1,4 @@
+import math
+
+a, b = [int(d) for d in input().split()]
+print(math.gcd(a, b))

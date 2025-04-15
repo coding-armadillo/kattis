3da84cee40a5daafccaa9632e92cf1ce8e428b67
Zeheng Li <imzehengl@gmail.com>
2025-04-15 08:11:30 -0600

    add examples

---
+++ src/dufuskuffur.lua
@@ -0,0 +1,8 @@
+n, m = io.read("*n", "*n")
+if n < m then
+    print('Dufur passa')
+elseif n > m then
+    print('Dufur passa ekki')
+else
+    print('Dufur passa fullkomlega')
+end
---
+++ src/dufuskuffur.py
@@ -0,0 +1,7 @@
+n, m = int(input()), int(input())
+if n < m:
+    print("Dufur passa")
+elif n > m:
+    print("Dufur passa ekki")
+else:
+    print("Dufur passa fullkomlega")
